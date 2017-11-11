911652fb2d2e35198d17fc8d906dfd92923e63b5
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2017-11-11 22:43:50 +0100

    Added: - Parameter for target bmc hostname - Auto detection feature for free ports. - Should allow to run multiple viewers simultaniously but java applet crashes.

--- bmc_mfles.sh
+++ bmc_mfles.sh
@@ -1,47 +1,72 @@
 #!/bin/bash
-#set -e
 
-NODE=pn01-bmc
-HOST=$NODE
+### user config ###
 
-INDEX=01
+# proxy server (ssh target)
+PROXY=fles
 
+# BMC config
+USER=ADMIN
+PW=ADMIN
 R_WEB=80
 R_SSL=443
 R_VNC=5900
 R_MEDIA=623
 
-L_WEB=$((1080 + $INDEX))
-L_SSL=$((1443 + $INDEX))
-L_VNC=$((6900 + $INDEX))
-L_MEDIA=$((1623 + $INDEX))
+### end user config ###
 
-cleanup () {
-    kill $SSH_PID
+set -e
+#set -x
+
+finish () {
+    kill $SSH_PID 2>/dev/null
     rm $COOKIE
     rm $JNLP
 }
-trap cleanup SIGINT SIGTERM SIGHUP
+trap finish EXIT
 
-echo "*** forward ports ***"
-ssh fles -N -L $L_WEB:$HOST:$R_WEB -L $L_SSL:$HOST:$R_SSL -L $L_VNC:$HOST:$R_VNC -L $L_MEDIA:$HOST:$R_MEDIA &
+if [ $# -ne 1 ]; then
+    echo "usagen: '$0 <hostname>'"
+    exit
+fi
+NODE=$1
+HOST=$NODE
+
+echo "*** finding unused ports ***"
+SUCCESS=0
+INDEX=0
+while [ $SUCCESS -ne 1 ]; do
+    L_WEB=$((1080 + $INDEX))
+    L_SSL=$((1443 + $INDEX))
+    L_VNC=$((6900 + $INDEX))
+    L_MEDIA=$((1623 + $INDEX))
+    if [ `nmap --open -p $L_WEB,$L_SSL,$L_VNC,$L_MEDIA localhost | wc -l` -ne 3 ]; then
+        INDEX=$(($INDEX + 1))
+        echo "*** trying again ..."
+    else
+        SUCCESS=1
+    fi
+done
+
+echo "*** forwarding ports ***"
+ssh $PROXY -N -L $L_WEB:$HOST:$R_WEB -L $L_SSL:$HOST:$R_SSL -L $L_VNC:$HOST:$R_VNC -L $L_MEDIA:$HOST:$R_MEDIA &
 SSH_PID=$!
 sleep 2
 
-echo "*** get a session ID ***"
+echo "*** getting a session ID ***"
 COOKIE=$(mktemp)
-wget --save-cookies $COOKIE --keep-session-cookies --post-data 'name=ADMIN&pwd=ADMIN' --delete-after http://localhost:$L_WEB/cgi/login.cgi
+wget --save-cookies $COOKIE --keep-session-cookies --post-data name=$USER\&pwd=$PW --delete-after http://localhost:$L_WEB/cgi/login.cgi
 SID=$(grep SID $COOKIE | awk '{print $7}')
 
-echo "*** get launch.jnlp ***"
+echo "*** getting launch.jnlp ***"
 JNLP=launch_$NODE.jnlp
-wget -O $JNLP.tmp --load-cookies $COOKIE --post-data 'url_name=sess_$SID&url_type=jwsk' http://localhost:$L_WEB/cgi/url_redirect.cgi
+wget -O $JNLP.tmp --load-cookies $COOKIE --post-data 'url_name=sess_&url_type=jwsk' http://localhost:$L_WEB/cgi/url_redirect.cgi
 
 # fix ports in jnlp
 sed -e s/localhost:$R_SSL/localhost:$L_SSL/g -e s/$R_VNC/$L_VNC/g -e s/$R_MEDIA/$L_MEDIA/g $JNLP.tmp > $JNLP
 rm $JNLP.tmp
 
-echo "*** launch KVM ***"
+echo "*** launching KVM ***"
 open $JNLP
 
 wait
